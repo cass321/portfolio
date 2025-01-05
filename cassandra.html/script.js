633d18e692c9cccf49e69ae117537d4c041eeb1b
cass321 <macassandra.paroni@evsu.edu.ph>
2025-01-05 21:56:13 +0800

    Add files via upload

---
+++ cassandra.html/script.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        title: "Project 1",
+        description: "JavaScript Assignment.",
+        image: "bg-img4.jpg"
+    },
+    {
+        title: "Project 2",
+        description: "Bio Data.",
+        image: "bg-img5.jpg"
+    },
+    {
+        title: "Project 3",
+        description: "my journey about creating this project.",
+        image: "bg-img6.jpg"
+    }
+];
+
+
+function showAlert() {
+    alert('Thank you for visiting my portfolio!');
+}
